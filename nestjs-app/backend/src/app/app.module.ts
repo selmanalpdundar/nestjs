8b2d2e4a96f3627ea394bfce35fc0566a6f00ea5
Selman Alpdundar <selman.alp@hotmail.com.tr>
2023-11-26 17:10:30 +0100

    Added Postgres Configurations and creating sample user entity

--- nestjs-app/backend/src/app/app.module.ts
+++ nestjs-app/backend/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from '../confıg/config.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
